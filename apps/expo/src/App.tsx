7518d7545698836e69c0f5d7993b955d4c7ef7c7
Eric Wooley <ericwooley@gmail.com>
2021-04-06 10:08:04 -0700

    expo constants bug

--- apps/expo/src/App.tsx
+++ apps/expo/src/App.tsx
@@ -2,18 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Ui } from '@rolledex/ui';
+import Constants from 'expo-constants';
 
 export default function App() {
   const [n, setn] = React.useState(0);
   React.useEffect(() => {
     const intTimer = setInterval(() => {
       setn(n + 1);
-    }, 10000);
+    }, 1000);
     return () => clearInterval(intTimer);
   }, []);
   return (
     <View style={styles.container}>
       <Text>{n}: Open up App to start working on your app!</Text>
+      <Text>
+        {n}: {JSON.stringify(Constants, null, 2)}
+      </Text>
       <Ui></Ui>
       <StatusBar style="auto" />
     </View>
